445e433ecf67d936d67e3f6b6f6189eadef37329
ART3MISTICAL <dev.marvel.avengers@gmail.com>
2022-06-24 18:20:26 +0530

    commit

--- test_main.py
+++ test_main.py
@@ -2,3 +2,7 @@ import main
 
 def test_encrypt():
 	assert main.encrypt('password') != 'password'
+
+def test_decrypt():
+	password = main.encrypt('password')
+	assert main.decrypt(password) == 'password'
