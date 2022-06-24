45eefb371fd715bfd8a6eaf4186330d2f4327e5a
ART3MISTICAL <dev.marvel.avengers@gmail.com>
2022-06-24 17:42:35 +0530

    commit

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ proceed = False
 def main():
 	# Main password to secure the script
 	root = input('Enter root password: ')
-	
+
 	# Enter any password of your choice
 	root_pass = '0'
-	
+
 	if root == root_pass:
 		proceed = True
 
+def prompt():
+	 prmpt = input('Press (1) to enter password or (2) to view passwords': )
+	 
