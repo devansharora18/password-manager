80f699b184a29564b42a7b6d29ce1538a6473417
ART3MISTICAL <dev.marvel.avengers@gmail.com>
2022-06-24 17:35:37 +0530

    commit

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from cryptography.fernet import Fernet
 
+key = Fernet.generate_key()
 
+cryption = Fernet(key)
 
 proceed = False
 
-# Main password to secure the script
-root = input('Enter root password: ')
-
-# Enter any password of your choice
-root_pass = '0'
-
-if root == root_pass:
-	proceed = True
+def main():
+	# Main password to secure the script
+	root = input('Enter root password: ')
+	
+	# Enter any password of your choice
+	root_pass = '0'
+	
+	if root == root_pass:
+		proceed = True
 
